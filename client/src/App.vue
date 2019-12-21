773aed8b157722b21a09e6df5204587d50ebfcd3
Alex.Prokofev <thetroller186@gmail.com>
2019-12-21 10:59:09 +0500

    vue router

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,14 +3,7 @@
     <v-app-bar app color="primary" dark></v-app-bar>
 
     <v-content>
-      <ul>
-        <li v-if="!isAuthenticated">
-          <a href="#" @click="login">Login</a>
-        </li>
-        <li v-if="isAuthenticated">
-          <a href="#" @click="logout">Log out</a>
-        </li>
-      </ul>
+      <router-view></router-view>
     </v-content>
   </v-app>
 </template>
@@ -19,28 +12,7 @@
 export default {
   name: "app",
   data() {
-    return {
-      isAuthenticated: false
-    };
-  },
-  async created() {
-    try {
-      await this.$auth.renewTokens();
-    } catch (e) {
-      alert(e);
-    }
-  },
-  methods: {
-    login() {
-      this.$auth.login();
-    },
-    logout() {
-      this.$auth.logOut();
-    },
-    handleLoginEvent(data) {
-      this.isAuthenticated = data.loggedIn;
-      this.profile = data.profile;
-    }
+    return {};
   }
 };
 </script>
